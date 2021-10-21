81517a3977c706ea17fea23713cc3334abe54e26
pater97 <giuseppepater997@gmail.com>
2021-10-21 12:53:58 +0200

    impostazioni base

---
+++ assets/javascript/main.js
@@ -0,0 +1,51 @@
+//CONSEGNA
+/*
+L'utente indica un livello di difficoltà in base al quale viene generata una griglia di gioco quadrata,
+in cui ogni cella contiene un numero tra quelli compresi in un range:
+con difficoltà 1 => tra 1 e 100
+con difficoltà 2 => tra 1 e 81
+con difficoltà 3 => tra 1 e 49
+*/
+
+//definire variabile di inserimento 
+const stampSquare = document.querySelector(".container")
+const playGame = document.querySelector('.controls > button')
+const gameDifficult = document.getElementById(`level`)
+//utilizzo di un condizionale per assegnare il numero di caselle in base al livello e ne stabilisco la grandezza
+if(gameDifficult.value == 1){
+    var numCaselle = 100
+    var sizeSquare = `one`
+}else if(gameDifficult.value == 2){
+    var numCaselle = 81
+    var sizeSquare = `two`
+}else if(gameDifficult.value == 3){
+    var numCaselle = 49
+    var sizeSquare = `trhee`
+}else{
+    alert(`!ATTENZIONE! il livello indicato non è disponibile`)
+}
+playGame.addEventListener(`click`,function(){
+    cycle(numCaselle)
+})
+//utilizzare una funzione per il ciclo for 
+function cycle (numero){
+    for (let i = 1; i <= numero;i++){
+        //creare un nuovo div
+        const square = document.createElement(`div`);
+        //creare variabile per inserimento testo
+        const text = document.createTextNode(i);
+        //inserire testo nel div
+        square.appendChild(text)
+        //aggiungere le classi da utilizzare
+        square.className=`square ${sizeSquare}`
+        //stampare il quadrato
+        stampSquare.append(square)
+        //al click aggiungere il background azzurro
+        square.addEventListener(`click`,function(){
+        square.style.backgroundColor="#85FFC7"
+    })
+    console.log(square);
+    }
+}
+//invocare la funzione
+
